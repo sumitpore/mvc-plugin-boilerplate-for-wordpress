8e05c46e212c2bcab434a838b8f743b450e4b0a7
Sumit Pore <sumit.pore@wisdmlabs.com>
2019-03-07 05:33:53 +0530

    Trait Registry - to be used to store multiple Controller and Router objects

---
+++ plugin-name/core/registry/plugin-name-registry-trait.php
@@ -0,0 +1,35 @@
+<?php
+
+if ( ! trait_exists( 'Plugin_Name_Registry_Trait' ) ) {
+
+	trait Plugin_Name_Registry_Trait {
+
+		protected static $stored_objects = [];
+
+		/**
+		 * @param string $key
+		 * @param mixed  $value
+		 *
+		 * @return void
+		 */
+		public static function set(string $key, $value)
+		{
+			static::$stored_objects[$key] = $value;
+		}
+
+		/**
+		 * @param string $key
+		 *
+		 * @return mixed
+		 */
+		public static function get(string $key)
+		{
+			if (!isset(static::$stored_objects[$key])) {
+				throw new \InvalidArgumentException('Invalid key given');
+			}
+
+			return static::$stored_objects[$key];
+		}
+	}
+
+}
