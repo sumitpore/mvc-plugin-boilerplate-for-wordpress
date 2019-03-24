42cd433a5ee8ef3bbda4c7f10da64762eb5543c0
Sumit Pore <sumit.pore@wisdmlabs.com>
2019-03-24 22:49:55 +0530

    Base Admin Controller

--- plugin-name/controllers/admin/class-plugin-name-controller-admin.php
+++ plugin-name/controllers/admin/class-plugin-name-controller-admin.php
@@ -6,13 +6,13 @@
  * @since      1.0.0
  * @package    Plugin_Name
  * @subpackage Plugin_Name/controllers/admin
- *
  */
 
 if ( ! class_exists( 'Plugin_Name_Controller_Admin' ) ) {
-
 	abstract class Plugin_Name_Controller_Admin extends Plugin_Name_Controller {
-
+		protected function __construct( $model_class_name = false, $view_class_name = false ) {
+			parent::__construct( $model_class_name, $view_class_name );
+		}
 	}
 
 }
