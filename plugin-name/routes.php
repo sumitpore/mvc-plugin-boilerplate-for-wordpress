4c0391b45bd97968c61e0d6a537e36592b472e33
Sumit Pore <sumit.pore@gmail.com>
2019-04-02 09:37:13 +0530

    Load Classes specified in Routes even if FQCN are not passed

--- plugin-name/routes.php
+++ plugin-name/routes.php
@@ -11,25 +11,47 @@
  * late_frontend - To be used if model/controller needs to be loaded when specific conditions are matched
  * late_frontend_with_possible_ajax - To be used if model/controller contains Ajax & needs to be loaded when specific conditions are matched
  */
+
+$router
+	->register_route_of_type( 'admin' )
+	->with_controller( 'Admin_Settings' )
+	->with_model( 'Admin_Settings' )
+	->with_view( 'Admin_Settings' );
+
+// Loading Route with full class names
+// $router
+// 	->register_route_of_type( 'admin' )
+// 	->with_controller( 'Plugin_Name\App\Controllers\Admin\Admin_Settings' )
+// 	->with_model( 'Plugin_Name\App\Models\Admin\Admin_Settings' )
+// 	->with_view( 'Plugin_Name\App\Views\Admin\Admin_Settings' );
+
+// Load controller if conditions match
 $router
 	->register_route_of_type( 'late_frontend' )
 	->with_controller(
 		function() {
 				// Load controller only if current post id is 3367
 			if ( get_the_ID() == '3367' ) {
-				  return 'Plugin_Name\App\Controllers\Frontend\Sample_Shortcode';
+				  return 'Sample_Shortcode';
 			}
 			return false;
 		}
 	)
-	->with_view( 'Plugin_Name\App\Views\Frontend\Sample_Shortcode' );
+	->with_view( 'Sample_Shortcode' );
 
-$router
-	->register_route_of_type( 'admin' )
-	->with_controller( 'Plugin_Name\App\Controllers\Admin\Admin_Settings' )
-	->with_model( 'Plugin_Name\App\Models\Admin\Admin_Settings' )
-	->with_view( 'Plugin_Name\App\Views\Admin\Admin_Settings' );
-
+// Load controller if conditions match with full class names
+// $router
+// 	->register_route_of_type( 'late_frontend' )
+// 	->with_controller(
+// 		function() {
+// 				// Load controller only if current post id is 3367
+// 			if ( get_the_ID() == '3367' ) {
+// 				  return 'Plugin_Name\App\Controllers\Frontend\Sample_Shortcode';
+// 			}
+// 			return false;
+// 		}
+// 	)
+// 	->with_view( 'Plugin_Name\App\Views\Frontend\Sample_Shortcode' );
 
 // If you want to load only model for specific route, you can use with_just_model
 // $router->register_route_of_type('admin')->with_just_model('Plugin_Name_Model_Admin_Settings');
