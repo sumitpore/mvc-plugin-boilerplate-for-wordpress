035da6e3cf24516399602a1fa2d03d235c78e20b
Sumit Pore <sumit.pore@gmail.com>
2019-04-11 08:13:50 +0530

    Modify the blueprint of Base Model & Base Controller. Methods which are not important in Core Model/Controller are moved to Base Model/Controller

--- plugin-name/app/models/admin/class-base-model.php
+++ plugin-name/app/models/admin/class-base-model.php
@@ -4,15 +4,29 @@ namespace Plugin_Name\App\Models\Admin;
 use \Plugin_Name\Core\Model;
 
 /**
- * Defines/implements base methods for admin model classes
+ * Blueprint for Admin related Models. All Admin Models should extend this Base_Model
  *
  * @since      1.0.0
  * @package    Plugin_Name
- * @subpackage Plugin_Name/models/admin
+ * @subpackage Plugin_Name/Models/Admin
  */
 
 if ( ! class_exists( __NAMESPACE__ . '\\' . 'Base_Model' ) ) {
 	abstract class Base_Model extends Model {
+
+		/**
+		 * Constructor
+		 *
+		 * @since    1.0.0
+		 */
+		abstract protected function __construct();
+
+		/**
+		 * Register callbacks for actions and filters
+		 *
+		 * @since    1.0.0
+		 */
+		abstract protected function register_hook_callbacks();
 
 	}
 
