ab50d5e07fd8efe88cf830986a877bc7f256f34a
Sumit Pore <sumit.pore@gmail.com>
2019-04-12 22:39:58 +0530

    Explain purpose of register_hook_callbacks method

--- plugin-name/app/controllers/frontend/class-base-controller.php
+++ plugin-name/app/controllers/frontend/class-base-controller.php
@@ -18,7 +18,19 @@ if ( ! class_exists( __NAMESPACE__ . '\\' . 'Base_Controller' ) ) {
 		}
 
 		/**
-		 * Register callbacks for actions and filters
+		 * Register callbacks for actions and filters. Most of your add_action/add_filter
+		 * go into this method.
+		 *
+		 * NOTE: register_hook_callbacks method is not called automatically. You
+		 * as a developer has to call this method where you see fit. For Example,
+		 * You may want to call this in constructor, if you feel hooks/filters
+		 * callbacks should be registered when the new instance of the class
+		 * is created.
+		 *
+		 * The purpose of this method is to set the convention that first place to
+		 * find add_action/add_filter is register_hook_callbacks method.
+		 *
+		 * @since    1.0.0
 		 */
 		abstract protected function register_hook_callbacks();
 	}
