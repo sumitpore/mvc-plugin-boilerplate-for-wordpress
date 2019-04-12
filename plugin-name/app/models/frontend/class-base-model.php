4545c53afa6245d7a9602c85208d592a0a4fdd72
Sumit Pore <sumit.pore@wisdmlabs.com>
2019-04-12 22:39:58 +0530

    Explain purpose of register_hook_callbacks method

--- plugin-name/app/models/frontend/class-base-model.php
+++ plugin-name/app/models/frontend/class-base-model.php
@@ -22,11 +22,29 @@ if ( ! class_exists( __NAMESPACE__ . '\\' . 'Base_Model' ) ) {
 		abstract protected function __construct();
 
 		/**
-		 * Register callbacks for actions and filters
+		 * Register callbacks for actions and filters. Most of your add_action/add_filter
+		 * go into this method.
+		 *
+		 * NOTE: register_hook_callbacks method is not called automatically. You
+		 * as a developer has to call this method where you see fit. For Example,
+		 * You may want to call this in constructor, if you feel hooks/filters
+		 * callbacks should be registered when the new instance of the class
+		 * is created.
+		 *
+		 * The purpose of this method is to set the convention that first place to
+		 * find add_action/add_filter is register_hook_callbacks method.
+		 *
+		 * This method is not marked abstract because it may not be needed in every
+		 * model. Making it abstract would enforce every child class to implement
+		 * the method.
+		 *
+		 * If I were you, I would define register_hook_callbacks method in the child
+		 * class when it is a 'Model only' route. This is not hard & fast rule, it
+		 * is just my opinion when I would define this method.
 		 *
 		 * @since    1.0.0
 		 */
-		abstract protected function register_hook_callbacks();
+		protected function register_hook_callbacks(){}
 
 	}
 }
