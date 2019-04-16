58634ea589a1c22bb2f25cc38afa8248b153c0f0
Sumit Pore <sumit.pore@wisdmlabs.com>
2019-04-16 08:33:11 +0530

    Update more information on how to use boilerplate

--- plugin-name/app/models/class-settings.php
+++ plugin-name/app/models/class-settings.php
@@ -25,7 +25,7 @@ if ( ! class_exists( __NAMESPACE__ . '\\' . 'Settings' ) ) {
 		protected static $settings;
 
 		/**
-		 * Retusn the Option name/key saved in the database
+		 * Returns the Option name/key saved in the database
 		 *
 		 * @return string
 		 * @since 1.0.0
